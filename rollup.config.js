47f3cb8f4d2a54c45315972dbac5e86e943e7002
Mike Bostock <mbostock@gmail.com>
2018-03-19 12:40:30 -0700

    Fix Rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 const definition = require("./package.json");
 
 export default {
-  input: "index",
+  input: "src/index.js",
   output: {
     banner: `// ${definition.homepage} Version ${definition.version}. Copyright 2018 Observable, Inc.`,
     file: `dist/${definition.name}.js`,
