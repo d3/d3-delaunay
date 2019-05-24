e440068b43a80cf0f128bf0dc66d1549ee87c2f0
Philippe Rivière <fil@rezo.net>
2019-05-24 10:15:26 +0200

    tests and make voronoi.render() return null for degenerate cases with less than 2 points (ie no triangulation)

--- src/voronoi.js
+++ src/voronoi.js
@@ -55,6 +55,7 @@ export default class Voronoi {
   render(context) {
     const buffer = context == null ? context = new Path : undefined;
     const {delaunay: {halfedges, inedges, hull}, circumcenters, vectors} = this;
+    if (hull.length <= 1) return null;
     for (let i = 0, n = halfedges.length; i < n; ++i) {
       const j = halfedges[i];
       if (j < i) continue;
